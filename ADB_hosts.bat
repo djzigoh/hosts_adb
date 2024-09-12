bfffcf121159c43affdedd98b3e0aee18d22c47e
djzigoh <djzigoh@gmail.com>
2024-09-11 21:57:40 -0300

    last

--- ADB_hosts.bat
+++ ADB_hosts.bat
@@ -1,7 +1,9 @@
 @echo off
 
 set URL=https://a.dove.isdumb.one/list.txt
-set OUTPUT_FILE=list.txt
+set GITHUB_USERNAME=djzigoh
+set GITHUB_REPO_NAME=hosts_adb
+
 cd c:\hosts_adb
 powershell -Command "Invoke-WebRequest -Uri %URL% -OutFile 03_2024_adb_block"
 powershell -Command "Invoke-WebRequest -Uri %URL% -OutFile 03_2024_adb_block_with_login"
@@ -16,14 +18,6 @@ powershell -Command "Invoke-WebRequest -Uri %URL% -OutFile 11_2024_adb_block_wit
 powershell -Command "Invoke-WebRequest -Uri %URL% -OutFile 12_2024_adb_block_with_login"
 
 
-
-
-
-set GITHUB_USERNAME=djzigoh
-set GITHUB_REPO_NAME=hosts_adb
-set OUTPUT_FILE=list.txt
-
-REM powershell -Command "git init"
 powershell -Command "git add ."
 powershell -Command "git commit -m "last" ."
 powershell -Command "git push"
