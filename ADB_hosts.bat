d331231d4de56af9dcbdfbfd646e3a2b75717fa0
djzigoh <djzigoh@gmail.com>
2024-09-11 21:53:16 -0300

    last

--- ADB_hosts.bat
+++ ADB_hosts.bat
@@ -3,7 +3,19 @@
 set URL=https://a.dove.isdumb.one/list.txt
 set OUTPUT_FILE=list.txt
 cd c:\hosts_adb
-powershell -Command "Invoke-WebRequest -Uri %URL% -OutFile %OUTPUT_FILE%"
+powershell -Command "Invoke-WebRequest -Uri %URL% -OutFile 03_2024_adb_block"
+powershell -Command "Invoke-WebRequest -Uri %URL% -OutFile 03_2024_adb_block_with_login"
+powershell -Command "Invoke-WebRequest -Uri %URL% -OutFile 04_2024_adb_block_with_login"
+powershell -Command "Invoke-WebRequest -Uri %URL% -OutFile 05_2024_adb_block_with_login"
+powershell -Command "Invoke-WebRequest -Uri %URL% -OutFile 06_2024_adb_block_with_login"
+powershell -Command "Invoke-WebRequest -Uri %URL% -OutFile 07_2024_adb_block_with_login"
+powershell -Command "Invoke-WebRequest -Uri %URL% -OutFile 08_2024_adb_block_with_login"
+powershell -Command "Invoke-WebRequest -Uri %URL% -OutFile 09_2024_adb_block_with_login"
+powershell -Command "Invoke-WebRequest -Uri %URL% -OutFile 10_2024_adb_block_with_login"
+powershell -Command "Invoke-WebRequest -Uri %URL% -OutFile 11_2024_adb_block_with_login"
+powershell -Command "Invoke-WebRequest -Uri %URL% -OutFile 12_2024_adb_block_with_login"
+
+
 
 
 
