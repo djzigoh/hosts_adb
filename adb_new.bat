834657e1b8adeac7e34e351c7da7efa74fed99d1
djzigoh <djzigoh@gmail.com>
2024-09-17 14:30:34 -0300

    17/09/2024

--- adb_new.bat
+++ adb_new.bat
@@ -8,10 +8,10 @@ set today=%date%
 
 cd c:\hosts_adb
 
-for /l %%a in (01,1,12) do (
+for /l %%a in (1,1,12) do (
   if %%a lss 10 (powershell -Command "Invoke-WebRequest -Uri %URL% -OutFile "0%%a_2024_adb_block_with_login"") else powershell -Command "Invoke-WebRequest -Uri %URL% -OutFile "%%a_2024_adb_block_with_login""
 )
 
 powershell -Command "git add ."
-powershell -Command "git commit -m "Updated blocklists for (%date%)" ."
+powershell -Command "git commit -m "%date%" ."
 powershell -Command "git push"
