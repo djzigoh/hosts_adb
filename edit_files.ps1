88d149f2c71428b3a8d1c4887257154367d19be3
djzigoh <djzigoh@gmail.com>
2024-09-25 09:39:39 -0300

    Last update

--- edit_files.ps1
+++ edit_files.ps1
@@ -2,7 +2,7 @@
 
 $INI = Get-Content -Path 'settings.ini' | Select-Object -Skip 1
 $directory = ($INI | Where-Object { $_.Trim() -match '^directory=' }) -replace '^directory=', ''
-$files = ($INI | Where-Object { $_.Trim() -match '^files=' }) -replace '^files=|$', ''
+$temp_list = ($INI | Where-Object { $_.Trim() -match '^temp_list=' }) -replace '^temp_list=|$', ''
 
 # Define your custom text (replace newline `r`n with your custom separator)
 $customText = @"
@@ -15,7 +15,7 @@ $customText = @"
 "@
 
 # Loop through all the .txt files in the directory
-Get-ChildItem "$directory\$files" | ForEach-Object {
+Get-ChildItem "$directory\$temp_list" | ForEach-Object {
     # Read the content of the file, skipping the first six lines
       $fileContent = Get-Content $_.FullName | Select-Object -Skip 6
 
